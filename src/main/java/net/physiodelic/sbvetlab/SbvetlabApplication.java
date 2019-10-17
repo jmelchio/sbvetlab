def94a22a3c41c873ce78370c800842cb91abe90
Joris Melchior <joris.melchior@gmail.com>
2019-10-16 22:20:52 -0400

    Various housekeeping items
    - .gitignore exclude .idea, .gradle and build
    - application.properties add connection information for development
    - apply proper indentation to generated files
    - make it possible to run application in external Servlet container

--- src/main/java/net/physiodelic/sbvetlab/SbvetlabApplication.java
+++ src/main/java/net/physiodelic/sbvetlab/SbvetlabApplication.java
@@ -2,12 +2,18 @@ package net.physiodelic.sbvetlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SbvetlabApplication {
+public class SbvetlabApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbvetlabApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SbvetlabApplication.class, args);
+  }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(SbvetlabApplication.class);
+  }
 }
