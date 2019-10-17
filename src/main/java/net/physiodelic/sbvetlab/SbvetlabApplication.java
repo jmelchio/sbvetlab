797c5c80bfef381f443538a7aa77a1aa5980c245
Joris Melchior <joris.melchior@gmail.com>
2019-10-16 21:16:29 -0400

    Local project creation from start.spring.io

---
+++ src/main/java/net/physiodelic/sbvetlab/SbvetlabApplication.java
@@ -0,0 +1,13 @@
+package net.physiodelic.sbvetlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbvetlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbvetlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/physiodelic/sbvetlab/SbvetlabApplicationTests.java
@@ -0,0 +1,13 @@
+package net.physiodelic.sbvetlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbvetlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
