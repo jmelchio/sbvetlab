f7e85320fa128de0dfa35c09f6b5a407f5c16674
Joris Melchior <joris.melchior@gmail.com>
2019-10-18 00:07:07 -0400

    Get working with persistence on Postgres
    - table with name 'user' should have name in quotes
    - hardcode table name as 'users' instead to avoid errors

--- src/main/java/net/physiodelic/sbvetlab/user/User.java
+++ src/main/java/net/physiodelic/sbvetlab/user/User.java
@@ -1,5 +1,6 @@
 package net.physiodelic.sbvetlab.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,14 +15,16 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "users")
 public class User {
   @Id
   @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Long id;
+  @Column(unique = true)
   private String userName;
   private String firstName;
   private String lastName;
+  @Column(name = "email")
   private String eMail;
   private String password;
   private Boolean adminUser;
