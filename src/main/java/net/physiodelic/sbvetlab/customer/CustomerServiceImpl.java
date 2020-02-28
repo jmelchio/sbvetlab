7a1c06ce2bee2b31d8057558462c6ad70f350a18
Joris Melchior <joris.melchior@gmail.com>
2020-02-28 08:06:27 -0500

    Add CustomerServiceImpl Class

---
+++ src/main/java/net/physiodelic/sbvetlab/customer/CustomerServiceImpl.java
@@ -0,0 +1,51 @@
+package net.physiodelic.sbvetlab.customer;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class CustomerServiceImpl implements CustomerService {
+  private CustomerRepository customerRepository;
+
+  public CustomerServiceImpl(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
+  @Override
+  public Customer login(String customerName, String password) {
+    return null;
+  }
+
+  @Override
+  public void update(Customer customer) {
+    customerRepository.save(customer);
+  }
+
+  @Override
+  public Customer create(Customer customer) {
+    return customerRepository.save(customer);
+  }
+
+  @Override
+  public void delete(Customer customer) {
+    customerRepository.delete(customer);
+  }
+
+  @Override
+  public Customer get(Long customerId) {
+    return customerRepository.findById(customerId).orElse(null);
+  }
+
+  @Override
+  public List<Customer> list(Map<String, Object> filter) {
+    return StreamSupport
+        .stream(customerRepository.findAll().spliterator(), false)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public Customer getByCustomerName(String name) {
+    return null;
+  }
+}
